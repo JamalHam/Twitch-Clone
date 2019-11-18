1baba283f18456cc87052f4828c2552423bdccc5
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-11-18 17:28:44 -0500

    google auth

--- client/src/components/streams/GoogleAuth.js
+++ client/src/components/streams/GoogleAuth.js
@@ -19,14 +19,33 @@ class GoogleAuth extends React.Component {
     onAuthChange = () => {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
     };
+
+    onSignInClick = () => {
+        this.auth.signIn();
+    }
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+    }
     
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
-            return <div>I dont know if we signed in</div>
+            return null
         }else if (this.state.isSignedIn) {
-            return <div> I am signed in</div>
+            return (
+                <button onClick={this.onSignOutClick} className="ui red google button">
+                    <i className="google icon" />
+                        Sign Out
+                    
+                </button>
+            );
         }else{
-            return<div>I am not signed in</div>
+            return (
+                <button onClick={this.onSignInClick} className="ui green google button">
+                    <i className="google icon" />
+                        Sign In with Google
+                </button>
+            );
         }
     }
 
