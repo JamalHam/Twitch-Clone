23645113b9b72be892bf722bd5cc820a679c8150
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-12-02 20:16:03 -0500

    fetching a specific stream

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import { 
     SIGN_IN, 
     SIGN_OUT, 
@@ -27,9 +28,10 @@ export const createStream = (formValues) => async (dispatch, getState) => {
     const response = await streams.post('/streams', { ...formValues, userId });
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
+    history.push('/');
 };
 
-export const fetchStreams = () => async dispatch => {
+export const fetchStreams = id => async dispatch => {
     const response = await streams.get('/streams');
 
     dispatch({ type: FETCH_STREAMS, payload: response.data });
